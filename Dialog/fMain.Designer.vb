504f813d8e7eea13dd01ad33612e883bdd8edd23
Yun Seok Choi <yschoi51@kict.re.kr>
2019-07-01 11:45:16 +0900

    GUI title was revised

--- Dialog/fMain.Designer.vb
+++ Dialog/fMain.Designer.vb
@@ -117,7 +117,7 @@ Partial Class fMain
         Me.MinimumSize = New System.Drawing.Size(503, 371)
         Me.Name = "fMain"
         Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
-        Me.Text = "Angel - v.2019.06.28"
+        Me.Text = "ALTEK - v.2019.06.28"
         Me.ResumeLayout(False)
 
     End Sub
