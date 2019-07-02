feacb56b80a62e0a45be2ddb2a934a004c70ee03
Yun Seok Choi <yschoi51@kict.re.kr>
2019-07-02 09:21:29 +0900

    executable files only

--- Dialog/fProgressBar.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class fProgressBar
-    Public Event StopProcess(ByVal sender As fProgressBar)
-
-    Private Sub btStop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btStop.Click
-        RaiseEvent StopProcess(Me)
-    End Sub
-
-End Class
